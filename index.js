b49f6dbe67611062027fb8f7d27cd0aa195e8d7e
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-08 21:11:59 +0500

    test

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
 app.use(express.json())
-require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_CONNECT_URL)
+mongoose.connect(process.env.MONGODB_CONNECT_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error !!!:'))
