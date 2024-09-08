989e177eed72ba6ca14610d45f41c1d3ea37d081
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-08 21:04:51 +0500

    test

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_CONNECT_URL)
 
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => console.log('Connected to MongoDB'))
+db.on('error', console.error.bind(console, 'connection error !!!:'))
+db.once('open', () => console.log('Connected to MongoDB !'))
 
 app.use("/product", require('./routes/products'))
 
