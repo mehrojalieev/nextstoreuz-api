1cc611aa54f1eabcadc66c332645b561ec479abe
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-09 23:30:04 +0500

    test

--- swagger.js
+++ swagger.js
@@ -7,20 +7,20 @@ const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'My API',
+      title: 'Product API',
       version: '1.0.0',
-      description: 'API hujjatlari va test',
+      description: 'Product API hujjatlari',
       contact: {
         name: 'Developer',
       },
       servers: [
         {
-          url: 'https://parfume-deploy-1.onrender.com', // Onlayn server URL
+          url: `http://localhost:${process.env.PORT || 8080}`, // Lokal server
         },
       ],
     },
   },
-  apis: ['./routes/*.js'], // API yo'llarini ko'rsating
+  apis: ['./routes/products.js'], // API yo'llarini ko'rsating
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
