8020042e8ff67e4ae5bc85f089f67e345d614d6f
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-16 23:08:27 +0500

    I changed Category Schema

--- models/products.js
+++ models/products.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose')
 
 // Category schema
 const categorySchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true
-    },
     name: {
         type: String,
         required: true
